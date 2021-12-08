87b02739dc04e612274b78ec1f13e7548d6f9676
eundol0519 <72530436+eundol0519@users.noreply.github.com>
2021-12-08 15:29:31 +0900

    로그인, 회원가입 뷰 수정

--- src/elements/Grid.js
+++ src/elements/Grid.js
@@ -7,6 +7,7 @@ import styled from "styled-components";
 const Grid = (props) => {
   const {
     is_flex,
+    commentWrite,
     width,
     height,
     margin,
@@ -16,12 +17,11 @@ const Grid = (props) => {
     center,
     _onClick,
     flexFlow,
-    justifyContent,
   } = props;
 
   const styles = {
-    justifyContent: justifyContent,
     is_flex: is_flex,
+    commentWrite : commentWrite,
     width: width,
     margin: margin,
     height: height,
@@ -40,11 +40,10 @@ const Grid = (props) => {
 };
 
 Grid.defaultProps = {
-  justifyContent: false,
   chidren: null,
   is_flex: false,
+  commentWrite : false,
   width: "100%",
-  height: "100%",
   padding: false,
   margin: false,
   bg: false,
@@ -62,14 +61,14 @@ const GridBox = styled.div`
     ${(props) => (props.bg ? `background-color: ${props.bg};` : "")}
     ${(props) =>
     props.is_flex
-      ? `display: flex; align-items: center; justify-content: space-evenly; `
+      ? `display: flex; align-items: center; justify-content: space-evenly;`
+      : ""}
+    ${(props) =>
+    props.commentWrite
+      ? `display: flex; align-items: center; `
       : ""}
     ${(props) => (props.center ? `text-align: center;` : "")}
     ${(props) => (props.flexFlow ? "flex-flow : row wrap;" : "")}
-    ${(props) =>
-    props.justifyContent
-      ? "justify-content: flex-start;"
-      : "justify-content: space-evenly;"}
 `;
 
 export default Grid;
